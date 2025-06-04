cfb9ad593310f6aacf5b7417b3b342fe13d65842
stephanedurette <Sdurette2688@conestogac.on.ca>
2025-06-04 16:13:06 -0400

    no message

--- Assets/_Project/Scripts/ScoreManager.cs
+++ Assets/_Project/Scripts/ScoreManager.cs
@@ -5,18 +5,26 @@ public class ScoreManager : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI highScoreText;
 
     [Header("Settings")]
     [SerializeField] private string scorePrefixString = "Score: ";
+    [SerializeField] private string highScorePrefixString = "Highscore: ";
 
     private int currentScore;
+    private int highScore;
 
     public int CurrentScore => currentScore;
+    public int HighScore => highScore;
 
     public void AddScore(int value)
     {
         currentScore += value;
         SetScoreText();
+        if (currentScore > highScore) { 
+            highScore = currentScore;
+            SetHighscoreText();
+        }
     }
 
     public void ResetScore()
@@ -28,6 +36,11 @@ public class ScoreManager : MonoBehaviour
     private void SetScoreText()
     {
         scoreText.text = $"{scorePrefixString}{currentScore}";
+    }
+
+    private void SetHighscoreText()
+    {
+        highScoreText.text = $"{highScorePrefixString}{highScore}";
     }
 
 
